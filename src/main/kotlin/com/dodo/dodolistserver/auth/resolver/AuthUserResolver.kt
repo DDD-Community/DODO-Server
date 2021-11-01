e6a9e7755cc8f6f4638455481b845bab84538f15
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-01 11:39:39 +0900

    Feat : UserAuthResolver 추가
    - 토큰을 검증하는 로직을 담은 Resolver
    - supportsParameter는 parameter의 type이 적절한지 검증해줍니다.

---
+++ src/main/kotlin/com/dodo/dodolistserver/auth/resolver/AuthUserResolver.kt
@@ -0,0 +1,57 @@
+package com.dodo.dodolistserver.auth.resolver
+
+import com.dodo.dodolistserver.user.repository.UserRepository
+
+import com.dodo.dodolistserver.auth.TokenProvider
+import com.dodo.dodolistserver.common.exception.AuthHeaderOmittedException
+import com.dodo.dodolistserver.common.exception.UserNotFoundException
+import com.dodo.dodolistserver.user.entity.User
+
+import lombok.RequiredArgsConstructor
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.core.MethodParameter
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import javax.servlet.http.HttpServletRequest
+
+private const val AUTH_TOKEN_HEADER = "Authorization"
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+class AuthUserResolver : HandlerMethodArgumentResolver {
+    private lateinit var httpServletRequest: HttpServletRequest
+    private lateinit var tokenProvider: TokenProvider
+    private lateinit var userRepository: UserRepository
+
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        val hasAnnotation = parameter.getParameterAnnotation(AuthUser::class.java) != null
+        val isMatchType: Boolean = (parameter.parameterType == User::class.java)
+        if (hasAnnotation && httpServletRequest.getHeader(AUTH_TOKEN_HEADER) == null) {
+            throw AuthHeaderOmittedException()
+        }
+        return hasAnnotation && isMatchType
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): User? {
+        val authToken: String = webRequest.getHeader(AUTH_TOKEN_HEADER) ?: return null
+
+        if (tokenProvider.isValidToken(authToken)) {
+            val userId = tokenProvider.extractIdByToken(authToken)
+            return userRepository.findByIdOrNull(userId)?: throw UserNotFoundException()
+        }
+
+        return null
+    }
+}
