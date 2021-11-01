17f69d4acb734d62bd49ecae9972e211252744c9
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-01 22:32:22 +0900

    Refactor : auth에 사용되는 Exception들 패키지 분리
    - 이후 handler의 분리를 위함.

--- src/main/kotlin/com/dodo/dodolistserver/common/exception/auth/AuthHeaderOmittedException.kt
+++ src/main/kotlin/com/dodo/dodolistserver/common/exception/auth/AuthHeaderOmittedException.kt
@@ -1,4 +1,4 @@
-package com.dodo.dodolistserver.common.exception
+package com.dodo.dodolistserver.common.exception.auth
 
 private const val AUTH_HEADER_OMITTED_EXCEPTION_MSG = "사용자 권한 요청정보가 없습니다."
 
--- src/main/kotlin/com/dodo/dodolistserver/common/exception/auth/FobiddenException.kt
+++ src/main/kotlin/com/dodo/dodolistserver/common/exception/auth/FobiddenException.kt
@@ -1,4 +1,4 @@
-package com.dodo.dodolistserver.common.exception
+package com.dodo.dodolistserver.common.exception.auth
 
 private const val FORIBIDDEN_EXCECPTION_MSG = "허용되지 않은 사용자 입니다."
 
