96066b8fdbac0ffce23ef899739ae727a02f1f80
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-06 18:52:52 +0900

    Refactor : 헤더 확인 로직 AuthUtils에서 담당하도록 수정

--- src/main/kotlin/com/dodo/dodolistserver/auth/util/AuthUtils.kt
+++ src/main/kotlin/com/dodo/dodolistserver/auth/util/AuthUtils.kt
@@ -1,7 +1,9 @@
 package com.dodo.dodolistserver.auth.util
 
+import com.dodo.dodolistserver.common.exception.auth.AuthHeaderOmittedException
 import lombok.AccessLevel
 import lombok.NoArgsConstructor
+import org.springframework.web.context.request.NativeWebRequest
 import javax.servlet.http.HttpServletRequest
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -10,5 +12,9 @@ object AuthUtils {
 
     fun extractToken(request: HttpServletRequest): String {
         return request.getHeader(AUTH_TOKEN_HEADER)?: throw AuthHeaderOmittedException()
+    }
+
+    fun extractTokenByWebRequest(webRequest: NativeWebRequest): String {
+        return webRequest.getHeader(AUTH_TOKEN_HEADER)?: throw AuthHeaderOmittedException()
     }
 }
