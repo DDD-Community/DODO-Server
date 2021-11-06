cd7787bc08509cb1f3e51946bec7c5aa564d0769
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-06 18:48:59 +0900

    Feat : email과 Type으로 유저를 찾을 수 있는 repository함수 추가

--- src/main/kotlin/com/dodo/dodolistserver/user/repository/UserRepository.kt
+++ src/main/kotlin/com/dodo/dodolistserver/user/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package com.dodo.dodolistserver.user.repository
 
 import com.dodo.dodolistserver.user.entity.User
+import com.dodo.dodolistserver.user.entity.UserType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User, Long> {
+    fun findByEmailAndType(email: String, type: UserType): User?
 }
