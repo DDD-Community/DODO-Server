9054e9a44c3969af8b8554d11aac8cc976adac35
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-06 20:39:31 +0900

    Fix : Merge Conflict 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,14 @@ repositories {
     mavenLocal()
 }
 
+// @Entity 클래스 바이트 코드 생성 시 인자 없는 기본 생성자 제네레이션을 위한 레퍼런스
+apply(plugin="org.jetbrains.kotlin.plugin.noarg")
+apply(plugin="org.jetbrains.kotlin.plugin.allopen")
+
 dependencies {
-    implementation("io.github.microutils:kotlin-logging:1.12.5")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
@@ -41,6 +45,7 @@ dependencies {
 
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -54,3 +59,14 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
